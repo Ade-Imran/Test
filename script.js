204fe44c9235e3e0916850af3472f46f877009c1
Ade-Imran <168285324+Ade-Imran@users.noreply.github.com>
2025-04-24 11:16:28 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,3 @@
-// Initialize Firebase
-const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",	
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID",
-  measurementId: "YOUR_MEASUREMENT_ID"
-};
-
-firebase.initializeApp(firebaseConfig);
-const db = firebase.firestore();
 
 // --- Firestore Helper Functions ---
 const BROWSER_ID = navigator.userAgent;
